df3a26c2ca2003336538203a4b3d1208bad46923
Deepak Singh <deepak0064.cse19@chitkara.edu.in>
2021-08-29 01:48:41 +0530

    bottom tabs

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,32 +8,53 @@ class TabScreens extends StatefulWidget {
 }
 
 class _TabScreensState extends State<TabScreens> {
+  final List<Widget> _pages = [
+    CategoriesScreen(),
+    FavouriteScreen(),
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Meals'),
-            bottom: TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.category_outlined),
-                  text: 'Categories',
-                ),
-                Tab(
-                  icon: Icon(Icons.star_border_outlined),
-                  text: 'Favourite',
-                )
-              ],
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Home',
+          style: TextStyle(
+            color: Color.fromRGBO(255, 72, 72, 1),
           ),
-          body: TabBarView(
-            children: [
-              CategoriesScreen(),
-              FavouriteScreen(),
-            ],
+        ),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+        elevation: 1,
+      ),
+      body: _pages[_selectedPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Colors.white,
+        elevation: 6,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Color.fromRGBO(255, 72, 72, 1),
+        currentIndex: _selectedPageIndex,
+        // type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category_outlined),
+            label: 'Categories',
           ),
-        ));
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star_border_outlined),
+            label: 'Favourites',
+          ),
+        ],
+      ),
+    );
   }
 }
